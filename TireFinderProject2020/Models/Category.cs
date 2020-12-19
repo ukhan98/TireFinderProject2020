817d12a2f21c511d5013c3631ce49e0a161e191d
Usman Khan <u.khan980212@gmail.com>
2020-12-19 00:31:30 -0500

    Added model classes 12 31 am usman

---
+++ TireFinderProject2020/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TireFinderProject2020.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public String CategoryName { get; set; }
+
+        public String CategoryDescription { get; set; }
+
+        public List<Tire> Tires { get; set; }
+
+    
+
+    }
+}
---
+++ TireFinderProject2020/Models/Tire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TireFinderProject2020.Models
+{
+    public class Tire
+    {
+        public int TireId { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public decimal Price { get; set; }
+        public String  ImageUrl { get; set; }
+        public String ImageThumbnailUrl { get; set; }
+        public bool IsOnSale { get; set; }
+        public bool IsInStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+    }
+}
+
